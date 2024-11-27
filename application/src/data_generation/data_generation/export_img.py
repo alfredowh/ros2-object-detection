d9a2d5acd365a690d952858c3fedc6ae6478c509
Alfredo Wijaya Hardjoprawiro <alfredo.hardjoprawiro@st.oth-regensburg.de>
2024-11-27 06:34:13 +0100

    Automate export images and driving

--- application/src/data_generation/data_generation/export_img.py
+++ application/src/data_generation/data_generation/export_img.py
@@ -3,8 +3,11 @@
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image
+from std_msgs.msg import Bool
 from cv_bridge import CvBridge
 from ament_index_python.packages import get_package_share_directory
+from rclpy.task import Future
+
 
 import cv2
 import os
@@ -15,6 +18,9 @@ class Img_exporter(Node):
 
     def __init__(self):
         super().__init__('img_exporter')
+
+        # Initialize indicating task completion
+        self.done_future = Future()
 
         self.img = None
         self.i = 0
@@ -34,17 +40,30 @@ class Img_exporter(Node):
             10)
         self.subscription 
 
+        self.driving_done_subscription = self.create_subscription(
+            Bool,
+            '/driving_done',
+            # '/rgbd_camera/image',
+            self.driving_done_callback,
+            10)
+        self.driving_done_subscription 
+
     def camera_callback(self, data):
         self.img = bridge.imgmsg_to_cv2(data, "bgr8")
 
+    def driving_done_callback(self, data):
+        self.done_future.set_result(data.data) 
+
     def timer_callback(self):
-        cv2.imwrite(os.path.join(self.export_dir, '00'+str(self.i)+'.jpeg'), self.img)
-        self.i += 1
+        if self.img is not None:
+            cv2.imwrite(os.path.join(self.export_dir, '00'+str(self.i)+'.jpeg'), self.img)
+            self.i += 1
 
 def main(args=None):
     rclpy.init(args=None)
     img_exporter = Img_exporter()
-    rclpy.spin(img_exporter)
+    # rclpy.spin(img_exporter)
+    rclpy.spin_until_future_complete(img_exporter, img_exporter.done_future)
     rclpy.shutdown()
 
 if __name__ == '__main__':
--- application/src/data_generation/data_generation/waypoints_driver.py
+++ application/src/data_generation/data_generation/waypoints_driver.py
@@ -4,15 +4,21 @@ import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
+from std_msgs.msg import Bool
 from tf_transformations import euler_from_quaternion
 from rclpy.task import Future
 
 class WaypointDriver(Node):
     def __init__(self):
         super().__init__('waypoint_driver')
+
+        self.declare_parameter('x_pose_init', 0.0)
+        self.declare_parameter('y_pose_init', 0.0)
+
+        self.driving_done_publisher = self.create_publisher(Bool, '/driving_done', 10)
         
         # Publisher for velocity commands
-        self.publisher = self.create_publisher(Twist, '/cmd_vel', 10)
+        self.controll_publisher = self.create_publisher(Twist, '/cmd_vel', 10)
         
         # Loop rate for driving controll 
         self.timer = self.create_timer(0.1, self.timer_callback)
@@ -28,20 +34,30 @@ class WaypointDriver(Node):
             10
         )
         
-        # Set waypoints (x, y)
+        # # Set waypoints (x, y)
         self.waypoints = [
-            {'x': 5.0, 'y': 0.0},
-            {'x': 5.0, 'y': 5.0},
-            {'x': 0.0, 'y': 0.0}
-        ]
+            {'x': -6.5, 'y': 9.2},
+            {'x': -3.7, 'y': 9.2},
+            {'x': -3.7, 'y': -9.44},
+            {'x': 1.77, 'y': -9.44},
+            {'x': 1.77, 'y': 2.24},
+            {'x': 6.22, 'y': 2.24}
+        ] # for test hall
+
         self.current_waypoint_index = 0
         self.robot_pose = {'x': 0.0, 'y': 0.0, 'yaw': 0.0}
-        self.velocity = 0.5 # m/s
+        self.velocity = .8 # m/s
         self.angular_gain = 1.0
 
     def pose_callback(self, msg):
-        self.robot_pose['x'] = msg.pose.pose.position.x
-        self.robot_pose['y'] = msg.pose.pose.position.y
+        x_pose_init = self.get_parameter('x_pose_init').get_parameter_value().double_value
+        y_pose_init = self.get_parameter('y_pose_init').get_parameter_value().double_value
+        self.robot_pose['x'] = msg.pose.pose.position.x + x_pose_init
+        self.robot_pose['y'] = msg.pose.pose.position.y + y_pose_init
+
+        # DEBUG
+        # self.get_logger().info(f"{x_pose_init}, {y_pose_init}")
+
         
         orientation_q = msg.pose.pose.orientation
         _, _, yaw = euler_from_quaternion([
@@ -58,9 +74,13 @@ class WaypointDriver(Node):
             twist = Twist()
             twist.linear.x = 0.0
             twist.angular.z = 0.0
-            self.publisher.publish(twist)
+            self.controll_publisher.publish(twist)
 
             self.done_future.set_result(True) 
+
+            driving_done = Bool()
+            driving_done.data = True
+            self.driving_done_publisher.publish(driving_done)
             return
 
         goal = self.waypoints[self.current_waypoint_index]
@@ -69,15 +89,18 @@ class WaypointDriver(Node):
         distance = math.sqrt(dx**2 + dy**2)
         angle_to_goal = math.atan2(dy, dx)
 
+        # DEBUG
+        # self.get_logger().info(f"Goal: ({goal['x']}, {goal['y']}), Pose: ({self.robot_pose['x']}, {self.robot_pose['y']}), Distance: {distance}, Yaw: {self.robot_pose['yaw']}, Angle to Goal: {angle_to_goal}\n")
+
         twist = Twist()
-        if distance > 0.1:  # Distance threshold to the waypoint
+        if distance > 0.2:  # Distance threshold to the waypoint
             twist.linear.x = self.velocity
             twist.angular.z = self.angular_gain * (angle_to_goal - self.robot_pose['yaw'])
         else:
             self.current_waypoint_index += 1
             self.get_logger().info(f'Waypoint ({str(goal["x"])}, {str(goal["y"])}) reached')
 
-        self.publisher.publish(twist)
+        self.controll_publisher.publish(twist)
 
 def main(args=None):
     rclpy.init(args=args)
